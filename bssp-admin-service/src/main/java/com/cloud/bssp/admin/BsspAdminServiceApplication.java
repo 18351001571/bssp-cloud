20b2f147fb2e1b9fdbe9e75f2a5be7b08207c3a2
魏荣鑫 <weirx@mvtech.com.cn>
2020-06-29 17:26:47 +0800

    调整结构

---
+++ bssp-admin-service/src/main/java/com/cloud/bssp/admin/BsspAdminServiceApplication.java
@@ -0,0 +1,23 @@
+package com.cloud.bssp.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+
+/**
+ * @description: 数据服务启动类
+ *
+ * 注解@ServletComponentScan： 使Servlet，filter，listener 可以通过 @WebServlet,@WebFilter,@WebListener
+ *                            等注解直接注册，无需其他代码
+ * @author weirx
+ * @date 2020/6/17
+ */
+@SpringBootApplication
+@ServletComponentScan
+public class BsspAdminServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BsspAdminServiceApplication.class, args);
+    }
+
+}
---
+++ bssp-admin-service/src/test/java/com/cloud/bssp/admin/BsspDataServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloud.bssp.admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BsspDataServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
