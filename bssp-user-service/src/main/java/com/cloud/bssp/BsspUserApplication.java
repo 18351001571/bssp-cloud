e544a749c3584b8aae78f3879913cc56b68c8c72
魏荣鑫 <weirx@mvtech.com.cn>
2021-03-24 09:44:02 +0800

    代码提交

--- bssp-user-service/src/main/java/com/cloud/bssp/BsspUserApplication.java
+++ bssp-user-service/src/main/java/com/cloud/bssp/BsspUserApplication.java
@@ -2,12 +2,15 @@ package com.cloud.bssp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 /**
  * 启动类
+ *
  * @author weirx
  */
 @SpringBootApplication
+@ComponentScan(value = "com.cloud.bssp.user")
 public class BsspUserApplication {
 
     public static void main(String[] args) {
